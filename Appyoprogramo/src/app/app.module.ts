f84833ac44031080175952980d0b8ffbc591d952
MarcosDavidPerez <marcoskun7@gmail.com>
2022-02-26 07:36:43 -0300

    segunda prueba

--- Appyoprogramo/src/app/app.module.ts
+++ Appyoprogramo/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogosproyectoComponent } from './components/logosproyecto/logosproyecto.component';
+import { ComunicacionComponent } from './components/comunicacion/comunicacion.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogosproyectoComponent
+    LogosproyectoComponent,
+    ComunicacionComponent,
+    BannerComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
